326dd71805625f59fc730c68318964b496172562
atahan.ozturk <ozturkatahan5@gmail.com>
2019-11-06 21:25:50 +0300

    yazıyı düzenleme

---
+++ BlogFall/Areas/Admin/ViewModels/PostEditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BlogFall.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogFall.Areas.Admin.ViewModels
+{
+    public class PostEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Başlık")]
+        public string Title { get; set; }
+
+        [Display(Name = "İçerik")]
+        public string Content { get; set; }
+
+
+    }
+}
